593150fc571b64853f1c0a85eb643e02753955ee
minghl <a879307253@gmail.com>
2023-11-19 12:31:12 +0100

    NGTW

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+// 和jquery引入不一样，lodash是直接import
+import * as _ from 'lodash';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'helloworld';
+  myModal: any;
+  ngOnInit(): void {
+    $('#bu').on('click',function() {
+      $('.aa').css('color', 'red');
+    })
+
+    _.isEmpty('')
+  }
 }
